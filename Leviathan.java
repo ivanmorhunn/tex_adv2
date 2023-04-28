e8a0005c0a988d2c7906b1708542ded23d101850
ivanmorhunn <121137716+ivanmorhunn@users.noreply.github.com>
2023-04-27 21:30:56 -0400

    Delete Leviathan.java

--- Leviathan.java
+++
@@ -1,7 +0,0 @@
-public class Leviathan extends Enemy{
-    private Powers powers;
-    public Leviathan(Powers powers){
-        super("Leviathan", 150, 35);
-        this.powers = powers;
-    }
-}
